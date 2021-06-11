657ccf7cb9d0fac23459ee52d1c3875f2cc2f252
lubmir2k <lubomir.mitana@gmail.com>
2021-06-11 13:55:23 +0200

    Added a placeholder to assets/

--- src/Chatter/Middleware/ImageRemoveExif.php
+++ src/Chatter/Middleware/ImageRemoveExif.php
@@ -11,9 +11,8 @@ class ImageRemoveExif
 		$newfile_type = $newfile->getClientMediaType();
 		$uploadFilename = $newfile->getClientFilename();
 		$newfile->moveTo("assets/images/tmp/$uploadFilename");
-		$pngfile = "assets/images/" . substr($uploadFilename, 0, -4) . "png"; // Drop the original extension and replace it with png
-		
-		// It is not a png, we need to write a new file
+		$pngfile = "assets/images/" . substr($uploadFilename, 0, -4) . ".png";
+
 		if('image/jpeg' == $newfile_type) {
 			$_img = imagecreatefromjpeg("assets/images/tmp/" . $uploadFilename);
 			imagepng($_img, $pngfile);
